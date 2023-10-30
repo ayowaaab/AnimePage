b367d0fa5ab3c11268bad91c8a34b0b584520ffc
ayowaaab <ayoub.dahmen@esprit.tn>
2023-10-30 16:17:58 +0100

    Added contact-icons

--- src/component/Nav.tsx
+++ src/component/Nav.tsx
@@ -1,13 +1,22 @@
-import {Button,HStack,Image,Stack,useBoolean,useColorMode} from "@chakra-ui/react";
+import {
+  Button,
+  HStack,
+  Image,
+  Stack,
+  useBoolean,
+  useColorMode,
+  Icon,
+  Text,
+  Link,
+} from "@chakra-ui/react";
 import logo from "../assets/Logo.svg";
-import { MoonIcon,SunIcon } from "@chakra-ui/icons";
-// import { MdSettings } from 'react-icons/md';
-
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { FaFacebook, FaGithub, FaInstagram } from "react-icons/fa";
 
 function Nav() {
   const { toggleColorMode } = useColorMode();
   const [flag, setFlag] = useBoolean();
-  
+
   return (
     <>
       <Stack
@@ -18,9 +27,16 @@ function Nav() {
         align={"center"}
         gap={5}
       >
-        <HStack gap={10}>
-          <Image src={logo} w={"50px"} />
-
+        <HStack gap={20}>
+          <HStack>
+            <Image src={logo} w={"50px"} />
+            <Text fontSize={'2xl'} as={'b'} color={'teal.700'}>Hope</Text>
+          </HStack>
+          <HStack gap={5}>
+            <Link opacity={'.5'} _hover={{opacity:'1'}} target="_blank" href='https://github.com/ayowaaab'><Icon cursor={"pointer"} as={FaGithub} boxSize={7} /></Link>
+            <Link opacity={'.5'} _hover={{opacity:'1'}} target="_blank" href='https://www.facebook.com/ayoub.dahmen.87'><Icon cursor={"pointer"} as={FaFacebook} boxSize={7} /></Link>
+            <Link opacity={'.5'} _hover={{opacity:'1'}} target="_blank" href='https://www.instagram.com/ayoub__dahmen/'><Icon cursor={"pointer"} as={FaInstagram} boxSize={7} /></Link>
+          </HStack>
         </HStack>
         <HStack gap={10}>
           <HStack>
@@ -30,13 +46,16 @@ function Nav() {
             <Button>Contanct</Button>
           </HStack>
           <HStack>
-            <Button onClick={() => {setFlag.toggle();toggleColorMode()}} >
-              {flag?<MoonIcon />:<SunIcon />}
+            <Button
+              onClick={() => {
+                setFlag.toggle();
+                toggleColorMode();
+              }}
+            >
+              {flag ? <MoonIcon /> : <SunIcon />}
             </Button>
-            {/* <Icon as={MdSettings} /> */}
             <Button>Sponsor ❤</Button>
           </HStack>
-     
         </HStack>
       </Stack>
       <hr />
